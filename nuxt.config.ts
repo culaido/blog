793b5f7f0489a0e13912bf4bed6408de432b7e1b
culaido <culaido@gmail.com>
2024-09-30 10:56:32 +0800

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,46 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { navbarData, seoData } from './data'
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+    app: {
+        head: {
+            charset: 'utf-8',
+            viewport: 'width=device-width,initial-scale=1',
+            title: navbarData.homeTitle,
+            titleTemplate: `%s - ${navbarData.homeTitle}`,
+        },
+        pageTransition: { name: 'page', mode: 'out-in' },
+        layoutTransition: { name: 'layout', mode: 'out-in' }
+    },
+    
+    site: {
+        url: 'https://abc.com',
+        identity: {
+            type: 'Person',
+        },
+    },
+  
+    colorMode: {
+        classSuffix: '',
+        preference: 'dark',
+        fallback: 'light',
+    },
+    
+    compatibilityDate: '2024-04-03',
+    devtools: { enabled: true },
+    modules: [
+        '@nuxt/icon',
+        '@nuxt/content',
+        '@vueuse/nuxt',
+        '@nuxtjs/color-mode',
+        '@nuxtjs/tailwindcss',
+        '@stefanobartoletti/nuxt-social-share',
+        'nuxt-og-image',
+        '@nuxt/image',
+        '@nuxtjs/robots'
+    ],
+    content: {
+        highlight: {
+            theme: 'dracula',
+        },
+    },
 })
